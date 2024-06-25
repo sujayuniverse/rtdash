934bd2999de972ae1b41400c1585065d6b078e04
sujayuniverse <sujayuniverse3@gmail.com>
2024-06-25 18:38:12 +0530

    exit app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { Platform, ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,9 +10,41 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   navigate: any;
-  constructor( private router: Router) {
+  private lastTimeBackPress = 0;
+  private timePeriodToExit = 2000;
+
+  constructor( private router: Router,  private platform: Platform,
+    private toastController: ToastController) {
     this.sideMenu();
+    this.initializeApp();
+
   }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      document.addEventListener('deviceready', () => {
+        this.handleBackButton();
+      }, false);
+    });
+  }
+
+  handleBackButton() {
+    document.addEventListener('backbutton', async () => {
+      const now = new Date().getTime();
+      if (now - this.lastTimeBackPress < this.timePeriodToExit) {
+        (navigator as any).app.exitApp(); // Close the app with type assertion
+      } else {
+        const toast = await this.toastController.create({
+          message: 'Press back again to exit',
+          duration: 2000,
+          position: 'bottom'
+        });
+        toast.present();
+        this.lastTimeBackPress = now;
+      }
+    }, false);
+  }
+
 
   sideMenu() {
     this.navigate =
