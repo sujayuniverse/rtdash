c8699d730ce4f053bfc13e5850f0577289f2284a
sujayuniverse <sujayuniverse3@gmail.com>
2024-07-02 11:15:06 +0530

    2ndjuly 1st commit with login implimentation

--- src/app/pages/login/login/login.page.ts
+++ src/app/pages/login/login/login.page.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { AlertController, LoadingController, NavController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { Router, NavigationExtras } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RestApiService } from 'src/app/rest-api.service';
+import { SetbaseurlService } from 'src/app/setbaseurl.service';
 
 @Component({
   selector: 'app-login',
@@ -10,40 +14,63 @@ import { ToastController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
-  EmailId: string = ""
-  password: string = ""
+  Username: string = "";
+  Password: string = "";
+  Site_url: string = "";
+  ReturnString: string = ""
+
   constructor(private router: Router,
     private alertCtrl: AlertController,
     public loadingController: LoadingController,
-    private toastController: ToastController) {
-
+    private toastController: ToastController,
+    private route: ActivatedRoute,
+    private ngZone: NgZone,
+    private http: HttpClient,
+    private baseurl: SetbaseurlService,
+    public restApiService: RestApiService,
+    private activatedRoute: ActivatedRoute) {
   }
 
 
 
   async Login() {
-    if (this.EmailId == '' || this.EmailId == null) {
-      this.showError('Please enter registered email id')
+    if ((this.Username == null || this.Username == '') ||
+      (this.Password == null || this.Password == '')) {
+      this.showError('Please fill the mandatory fields.')
     }
     else {
-      const loading = await this.loadingController.create({
-        message: 'Please wait',
-        duration: 10000
-      });
-      await loading.present();
-      if (this.EmailId == "admin@gmail.com" && this.password == "1234") {
-        this.router.navigate(['/dashboard']);
-        this.EmailId = '';
-        this.password = '';
-      }
-      else {
-        this.EmailId = '';
-        this.password = '';
-        this.showError('Please enter valid credentials')
-      }
-      loading.dismiss()
+      if (this.Username != "" && this.Username != "") {
+        const loading = await this.loadingController.create({
+          message: 'Please wait',
+          duration: 7000
+        });
+        await loading.present();
 
+        var dataToSend =
+        {
+          "Username": this.Username,
+          "Password": this.Password,
+        };
+
+        this.restApiService.SaveUser(dataToSend).subscribe(dataReturnFromService => {
+          console.log(dataReturnFromService)
+          if (dataReturnFromService == "Valid") {
+            loading.dismiss();
+            this.showError('Welcome ' + this.Username)
+            this.Username = "";
+            this.Password = "";
+            this.router.navigate(['/dashboard']);
+          }
+          else {
+            this.showError('Invalid credentials. Please try again later.')
+            this.Username = "";
+            this.Password = "";
+            loading.dismiss()
+          }
+        })
+      }
     }
+
   }
 
   async showError(message: string) {
@@ -54,6 +81,42 @@ export class LoginPage implements OnInit {
     toast.present();
 
   }
+
+
+
+  // async Login() {
+  //   if (this.EmailId == '' || this.EmailId == null) {
+  //     this.showError('Please enter registered email id')
+  //   }
+  //   else {
+  //     const loading = await this.loadingController.create({
+  //       message: 'Please wait',
+  //       duration: 10000
+  //     });
+  //     await loading.present();
+  //     if (this.EmailId == "admin@gmail.com" && this.password == "1234") {
+  //       this.router.navigate(['/dashboard']);
+  //       this.EmailId = '';
+  //       this.password = '';
+  //     }
+  //     else {
+  //       this.EmailId = '';
+  //       this.password = '';
+  //       this.showError('Please enter valid credentials')
+  //     }
+  //     loading.dismiss()
+
+  //   }
+  // }
+
+  // async showError(message: string) {
+  //   const toast = await this.toastController.create({
+  //     message: message,
+  //     duration: 3000,
+  //   });
+  //   toast.present();
+
+  // }
 
   ngOnInit() {
   }
--- src/app/rest-api.service.ts
+++ src/app/rest-api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestApiService {
+  base_url = 'https://ccmobuat.ucb.com.bd:8443/api/';
+  //'http://localhost:62581/api/';
+  constructor(public http: HttpClient)
+  {
+ 
+   }
+  
+  SaveUser(dataToSend:any){
+    let url = this.base_url + 'get_login';
+    return this.http.post(url, dataToSend,
+      {
+        headers: new HttpHeaders(
+        {'content-Type': 'application/json'}
+        )
+      }
+      );
 
-  constructor() { }
+  }
+
+  
 }
